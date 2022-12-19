770a10affb35127670fed646ca7a5a9970addfc8
reneenh <reneehamlinteaches@gmail.com>
2022-12-18 21:15:02 -0800

    refactored LoginForm.js

--- Develop/client/src/components/SignupForm.js
+++ Develop/client/src/components/SignupForm.js
@@ -1,8 +1,7 @@
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 
-//remove createUser from API and replace with ADD_USER mutation
-import { createUser } from '../utils/API';
+import { ADD_USER } from "../utils/mutations";
 import Auth from '../utils/auth';
 
 const SignupForm = () => {
