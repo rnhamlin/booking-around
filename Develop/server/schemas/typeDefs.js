db6362d8907dae840500343c2666c7ed5321193d
reneenh <reneehamlinteaches@gmail.com>
2022-12-18 16:07:12 -0800

    fixed typeDefs

--- Develop/server/schemas/typeDefs.js
+++ Develop/server/schemas/typeDefs.js
@@ -4,13 +4,35 @@ const { gql } = require("apollo-server-express");
 // create our typeDefs
 const typeDefs = gql`
   type Query {
-    me: String
+    me: User
   }
+
   type Mutation {
-    login: String
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(id: String, author: String): User
   }
-  type: Mutation {
-    addUser: String,
+
+  type User {
+    _id: String
+    username: String!
+    email: String!
+    bookCount: Int
+    savedBooks: [Book]
+  }
+
+  type Book {
+    bookId: String
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
+  }
+
+  type Auth {
+    token: ID
+    user: User
   }
 `;
 
--- Develop/server/server.js
+++ Develop/server/server.js
@@ -1,21 +1,50 @@
-const express = require('express');
-const path = require('path');
-const db = require('./config/connection');
-const routes = require('./routes');
+const express = require("express");
 
-const app = express();
+// Import ApolloServer
+const { ApolloServer } = require("apollo-server-express");
+// Import the typeDefs and resolvers
+const { typeDefs, resolvers } = require("./schemas");
+const db = require("./config/connection");
+
 const PORT = process.env.PORT || 3001;
 
-app.use(express.urlencoded({ extended: true }));
+//Now, create a new Apollo server and pass it into the schema data.
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+//const path = require('path');
+//const routes = require('./routes');
+
+const app = express();
+
+// `extended` below is true in the original, but in the example in BCS, it's false.
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// Create a new instance of an Apollo server with the GraphQL schema
+const startApolloServer = async (typeDefs, resolvers) => {
+  await server.start();
+  //integrate Apollo server with Express application as middleware.
+  server.applyMiddleware({ app });
+
+  db.once("open", () => {
+    app.listen(PORT, () => {
+      console.log(`🌍 Now listening on localhost:${PORT}`);
+      //log where we can go to test our GQL API
+      console.log(
+        `Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`
+      );
+    });
+  });
+};
+// Calls the async function to start the server:
+startApolloServer(typeDefs, resolvers);
+
 // if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "../client/build")));
+// }
 
-app.use(routes);
-
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-});
+//app.use(routes);
